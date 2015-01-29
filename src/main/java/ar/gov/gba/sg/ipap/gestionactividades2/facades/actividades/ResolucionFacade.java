8aef0cdc62a1e045171f9c8f539c40d3206170af
Ruben Incostante <rincostante@ambiente.gob.ar>
2015-01-29 19:08:37 -0300

    Se completó la administración de Sedes y se comenzó con el menú Actividades, Resoluciones.

--- src/main/java/ar/gov/gba/sg/ipap/gestionactividades2/facades/actividades/ResolucionFacade.java
+++ src/main/java/ar/gov/gba/sg/ipap/gestionactividades2/facades/actividades/ResolucionFacade.java
@@ -7,9 +7,11 @@
 package ar.gov.gba.sg.ipap.gestionactividades2.facades.actividades;
 
 import ar.gov.gba.sg.ipap.gestionactividades2.entities.actividades.Resolucion;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
@@ -28,5 +30,82 @@ public class ResolucionFacade extends AbstractFacade<Resolucion> {
     public ResolucionFacade() {
         super(Resolucion.class);
     }
+  
+    /**
+     * Método que verifica si la Resolucion puede ser eliminada
+     * @param id: Id de la Resolucion que se desea verificar
+     * @return
+     */
+    public boolean getUtilizado(Long id){
+        em = getEntityManager();
+        String queryString = "SELECT actImp.nombre FROM ActividadImplementada actImp "
+                + "INNER JOIN actImp.actividadPlan actPlan "
+                + "INNER JOIN actPlan.subprogramas sub "
+                + "INNER JOIN sub.programa prog "
+                + "WHERE prog.resolucion.id = :id "
+                + "OR sub.resolucion.id = :id "
+                + "OR actPlan.resolucion.id = :id "
+                + "OR actImp.resolucion.id = :id";
+        Query q = em.createQuery(queryString)
+                .setParameter("id", id);
+        return q.getResultList().isEmpty();
+    } 
     
+    /**
+     * Método para validad que no exista una Resolucion con este nombre ya ingresada
+     * @param res
+     * @param anio
+     * @return 
+     */
+    public boolean noExiste(String res, int anio){
+        em = getEntityManager();
+        String queryString = "SELECT res FROM Resolucion res "
+                + "WHERE res.resolucion = :res "
+                + "res.anio = :anio";
+        Query q = em.createQuery(queryString)
+                .setParameter("res", res)
+                .setParameter("anio", anio);
+        return q.getResultList().isEmpty();
+    }
+    
+    /**
+     * Método que obtiene una Resolucion existente según los datos recibidos como parámetro
+     * @param res
+     * @param anio
+     * @return 
+     */
+    public Resolucion getExistente(String res, int anio){
+        em = getEntityManager();
+        String queryString = "SELECT res FROM Resolucion res "
+                + "WHERE res.resolucion = :res "
+                + "res.anio = :anio";
+        Query q = em.createQuery(queryString)
+                .setParameter("res", res)
+                .setParameter("anio", anio);
+        return (Resolucion)q.getSingleResult();
+    }    
+    
+    /**
+     * Método que devuelve todas las Resoluciones habilitadas
+     * @return 
+     */
+    public List<Resolucion> getHabilitadas(){
+        em = getEntityManager();
+        String queryString = "SELECT res FROM Resolucion res "
+                + "WHERE res.admin.habilitado = true";
+        Query q = em.createQuery(queryString);
+        return q.getResultList();
+    }
+    
+    /**
+     * Método que devuelve todas las Resoluciones deshabilitadas
+     * @return 
+     */
+    public List<Resolucion> getDeshabilitadas(){
+        em = getEntityManager();
+        String queryString = "SELECT res FROM Resolucion res "
+                + "WHERE res.admin.habilitado = false";
+        Query q = em.createQuery(queryString);
+        return q.getResultList();
+    }        
 }
--- src/main/java/ar/gov/gba/sg/ipap/gestionactividades2/facades/actividades/SedeFacade.java
+++ src/main/java/ar/gov/gba/sg/ipap/gestionactividades2/facades/actividades/SedeFacade.java
@@ -7,9 +7,11 @@
 package ar.gov.gba.sg.ipap.gestionactividades2.facades.actividades;
 
 import ar.gov.gba.sg.ipap.gestionactividades2.entities.actividades.Sede;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
@@ -28,5 +30,71 @@ public class SedeFacade extends AbstractFacade<Sede> {
     public SedeFacade() {
         super(Sede.class);
     }
+ 
+    /**
+     * Método que verifica si la Sede puede ser eliminada
+     * @param id: Id de la Sede que se desea verificar
+     * @return
+     */
+    public boolean getUtilizado(Long id){
+        em = getEntityManager();
+        String queryString = "SELECT sede.nombre FROM Sede sede "
+                + "INNER JOIN sede.actividades actImp "
+                + "WHERE sede.id = :id ";
+        Query q = em.createQuery(queryString)
+                .setParameter("id", id);
+        return q.getResultList().isEmpty();
+    } 
     
+    /**
+     * Método para validad que no exista una Sede con este nombre ya ingresada
+     * @param nombre: nombre de la Sede
+     * @return 
+     */
+    public boolean noExiste(String nombre){
+        em = getEntityManager();
+        String queryString = "SELECT sede FROM Sede sede "
+                + "WHERE sede.nombre = :nombre";
+        Query q = em.createQuery(queryString)
+                .setParameter("nombre", nombre);
+        return q.getResultList().isEmpty();
+    }
+    
+    /**
+     * Método que obtiene una Sede existente según los datos recibidos como parámetro
+     * @param nombre: nombre de la Sede
+     * @return 
+     */
+    public Sede getExistente(String nombre){
+        em = getEntityManager();
+        String queryString = "SELECT sede FROM Sede sede "
+                + "WHERE sede.nombre = :nombre";
+        Query q = em.createQuery(queryString)
+                .setParameter("nombre", nombre);
+        return (Sede)q.getSingleResult();
+    }    
+    
+    /**
+     * Método que devuelve todas las Sedes habilitadas
+     * @return 
+     */
+    public List<Sede> getHabilitados(){
+        em = getEntityManager();
+        String queryString = "SELECT sede FROM Sede sede "
+                + "WHERE sede.admin.habilitado = true";
+        Query q = em.createQuery(queryString);
+        return q.getResultList();
+    }
+    
+    /**
+     * Método que devuelve todas las Sedes deshabilitadas
+     * @return 
+     */
+    public List<Sede> getDeshabilitados(){
+        em = getEntityManager();
+        String queryString = "SELECT sede FROM Sede sede "
+                + "WHERE sede.admin.habilitado = false";
+        Query q = em.createQuery(queryString);
+        return q.getResultList();
+    }    
 }
